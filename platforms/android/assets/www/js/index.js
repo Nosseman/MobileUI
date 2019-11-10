710563ce5c81a18b6f4e36e7a5a2756306c99bd0
Nosseman <juuso.nousiainen@hotmail.com>
2019-11-10 20:04:40 +0200

    nasa api

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,3 +1,39 @@
+var app = {
+    initialize: function () {
+
+        var url = "https://api.nasa.gov/planetary/apod?api_key=lCA7zRfS7C3DRA8FSqhoHeM4iGzxACEm3yyXbUuy";
+
+        $("#randomImage").click(function () {
+            $.ajax({
+                url: url + "&date=" + randomDate(new Date(2015, 0, 1), new Date()),
+                success: handleResult
+            });
+
+            function handleResult(result) {
+                $("#spaceimage").attr("src", result.url);
+
+                $("#spaceimage").responsiveImg();
+
+                $("#copyright").text("Copyright: " + result.copyright);
+                $("#desc").text(result.explanation);
+            }
+        });
+    },
+};
+
+app.initialize();
+
+// based on https://gist.github.com/miguelmota/5b67e03845d840c949c4
+function randomDate(start, end) {
+    var date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+
+    var dat = date.getDate();
+    var month = date.getMonth() + 1;
+    var yr = date.getFullYear();
+
+    return yr + "-" + month + "-" + dat;
+}
+
 $(document).on("ready", function () {
     databaseHandler.createDatabase();
 });
@@ -25,4 +61,13 @@ function login() {
     var userid = "" + username + password
     userHandler.getUser(userid);
 }
+
+document.addEventListener('openPage', function (e) {
+    if (e.detail.page == 'page1.html') {
+        new Swiper('.swiper-container', {
+            pagination: '.spiwer-pagination',
+            paginationClickable: true
+        });
+    }
+})
 
